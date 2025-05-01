6d9c4fd0170f9aba72f0cad3eb0ec03c32d0cfdc
avikram553 <avikram553@gmail.com>
2025-05-01 16:30:06 +0530

    Python course added

--- python/pyhon.py
+++ python/pyhon.py
@@ -1,1 +1,12 @@
-print(2+2)
+'''
+Just a simple line to cooment multistatement
+
+'''
+
+
+age =int(input("Enter your age: "))
+if age > 18:
+    print("Adult")
+else:
+    print("Not Adult" + str(age))
+          
