41d9cd570d8265f407c5099644bd72bc078ecebd
avikram553 <avikram553@gmail.com>
2025-05-03 20:32:02 +0530

    FileHandling and Libraries

---
+++ python/package/math.py
@@ -0,0 +1,5 @@
+def multiply(a,b):
+    return a*b
+
+is_even=lambda x : x%2==0
+is_divisible= lambda y,n: y%n==0
--- python/pyhon.py
+++ python/pyhon.py
@@ -3,10 +3,24 @@ Just a simple line to cooment multistatement
 
 '''
 
+import package
 
-age =int(input("Enter your age: "))
+from package import math
+
+print(math.is_even(5678
+                   ))
+
+print(math.is_divisible(98,7))
+
+'''age =int(input("Enter your age: "))
 if age > 18:
     print("Adult")
 else:
-    print("Not Adult" + str(age))
-          
+
+
+    print("Not Adult" + ' ' str(age))
+
+'''
+
+
+
